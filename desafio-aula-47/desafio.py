019eb914a7edfaf81c04fc040a7a7532397f93cf
Higor Nóbrega <higorst.nobrega@gmail.com>
2022-03-20 18:24:43 -0300

    desafio contador

---
+++ desafio-aula-47/desafio.py
@@ -0,0 +1,16 @@
+
+
+cont_menos = 10
+cont_mais = 0 
+# while cont_menos >= 0:
+#     print(f'{cont_menos} {cont_mais}')
+#     cont_menos -= 1
+#     cont_mais += 1
+
+# for cont_menos in range(11):
+#     print(f'{cont_menos} {10 - cont_menos}')
+
+'''Sujestão do professor'''
+
+for r, p in enumerate(range(10, 1, -1)):
+    print(r, p)
