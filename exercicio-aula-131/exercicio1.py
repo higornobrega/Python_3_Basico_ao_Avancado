735aad9480ac24997a92f86467fb416f9d945096
Higor Nóbrega <higorst.nobrega@gmail.com>
2023-09-26 13:51:57 -0300

    resolução do exercício

--- exercicio-aula-131/exercicio1.py
+++ exercicio-aula-131/exercicio1.py
@@ -12,7 +12,7 @@ Requisitos:
     Se não encontrar duplicados na lista, retorne -1
 """
 lista_de_listas_de_inteiros = [
-    # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
     [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
     [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
@@ -25,16 +25,24 @@ lista_de_listas_de_inteiros = [
     [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
     [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
 ]
-index = []
-for lista_de_listas_de_inteiro in lista_de_listas_de_inteiros:
-    for lista in lista_de_listas_de_inteiro:
-        print(lista)
-        print(lista_de_listas_de_inteiro)
-        if lista_de_listas_de_inteiro.count(lista) > 1:
 
-            for x in range(len(lista_de_listas_de_inteiro)):
-                t = lista_de_listas_de_inteiro[x]
-                if t == lista:
-                    index.append(x)
-        print(index)
-        index = []
+
+def encontra_primeiro_duplicado(lista_de_inteiros):
+    numeros_checados = set()
+    primeiro_duplicado = -1
+
+    for numero in lista_de_inteiros:
+        if numero in numeros_checados:
+            primeiro_duplicado = numero
+            break
+
+        numeros_checados.add(numero)
+
+    return primeiro_duplicado
+
+
+for lista in lista_de_listas_de_inteiros:
+    print(
+        lista,
+        encontra_primeiro_duplicado(lista)
+    )
