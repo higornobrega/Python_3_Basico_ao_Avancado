f0d5fd6b988a7ec03663a5241939a74e705e87ef
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-02-13 09:26:16 -0300

    módulo

--- aula154/modulo1.py
+++ aula154/modulo1.py
@@ -1,3 +1,13 @@
+import importlib  # Para re-importar um módulo
+
 import modulo2
 
 print('Arquivo modulo1 - Esse módulo é o ', __name__)
+
+# Re-importando módulos
+
+for i in range(10):
+    importlib.reload(modulo2)
+    print(i)
+
+print('fim')
